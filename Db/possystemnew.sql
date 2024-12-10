b6b720a702244a75480dfd37d63a0fbe3194afa7
buddhikaeshan <buddhikaeshan12@gmail.com>
2024-12-10 23:06:36 +0530

    env and database

--- Db/possystemnew.sql
+++ Db/possystemnew.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2024 at 02:06 PM
+-- Generation Time: Dec 10, 2024 at 06:34 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `possystem`
+-- Database: `possystemnew`
 --
 
 -- --------------------------------------------------------
