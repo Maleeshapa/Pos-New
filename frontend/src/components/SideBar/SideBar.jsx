94360ba262f2b65b0edb531b86523a061be4c319
Maleeshapa <maleeshapathirana1@gmail.com>
2024-12-17 22:06:00 +0530

    'porforma'

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -78,6 +78,7 @@ const Sidebar = () => {
                 { title: 'Delivery', path: '/sales/delivery' },
                 { title: 'Draft', path: '/sales/draft' },
                 { title: 'Credit', path: '/sales/credit' },
+                { title: 'Proforma Invoice', path: '/sales/ProformaInvoice' },
             ]
         },
         /*{
